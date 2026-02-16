1b06f456fe3d55b136d191fdd8e7a7fdda37ff2f
prathamesh Nandgirwar <123444895+prathamnandgirwar@users.noreply.github.com>
2026-02-16 16:20:48 +0530

    Create security.tf

---
+++ terraform-lb-asg/security.tf
@@ -0,0 +1,27 @@
+# Security Group for EC2 and Load Balancer
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # SSH access
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # HTTP access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
